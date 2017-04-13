d8d35d2fe8116c701151d929c9d5b84cb4b8cc40
Ryan Donovan <ryandonovan@d-172-25-45-220.dhcp.virginia.edu>
2017-04-13 17:38:15 -0400

    Integrated front end for accounts

--- Fintech/Fintech/urls.py
+++ Fintech/Fintech/urls.py
@@ -4,20 +4,21 @@ from account import views as account_views
 from reports import views as reports_views
 from groups import views as group_views
 
-
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'Fintech.views.home', name='home'),
+    #url(r'^$', 'Fintech.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', account_views.home, name='home'),
+    url(r'^investor_info/', account_views.investor_info, name='investor_info'),
+    url(r'^company_info/', account_views.company_info, name='company_info'),
     url(r'^signup/', account_views.signupform, name='signup'),
-    url(r'^login/', account_views.log),
+    url(r'^login/', account_views.log, name='login'),
     url(r'^auth/', account_views.auth),
     url(r'^showdata/', account_views.showdata),
-    url(r'^logout/', account_views.loggedout),
-    url(r'^loggedin/', account_views.loggedin),
+    url(r'^logout/', account_views.loggedout, name = 'logout'),
+    url(r'^loggedin/', account_views.loggedin, name='loggedin'),
     url(r'^invalid/', account_views.invalid),
-    url(r'^reports/', include('reports.urls', namespace='reports')),
-    url(r'^group/', group_views.submit_group)
+    url(r'^reports/', include('reports.urls', namespace='reports'),
+    url(r'^group/', group_views.submit_group))
 ]
--- Fintech/account/views.py
+++ Fintech/account/views.py
@@ -29,7 +29,7 @@ def signupform(request):
             uinfo.user = user
             uinfo.user_type = user_type
             uinfo.save()
-            return render(request, 'login.html')  # Redirect after POST
+            return render(request, 'html5up/login.html')  # Redirect after POST
 
             # return render(request, 'result.html', {
             # 		'username': form.cleaned_data['username'],
@@ -40,18 +40,32 @@ def signupform(request):
         form = SignupForm()
 
     #returning form
-    return render(request, 'signupform.html', {'form':form})
+    return render(request, 'html5up/signup.html', {'form':form})
 
 @login_required
 def showdata(request):
     all_users = User.objects.all()
     return render(request, 'showdata.html', {'all_users': all_users, })
 
+def home(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'html5up/index.html', c)
+
+def investor_info(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'html5up/investor_info.html', c)
+
+def company_info(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'html5up/company_info.html', c)
 
 def log(request):
     c = {}
     c.update(csrf(request))
-    return render(request, 'login.html', c)
+    return render(request, 'html5up/login.html', c)
 
 def auth(request):
     username = request.POST['username']
@@ -67,13 +81,18 @@ def auth(request):
 def loggedin(request):
     c = {}
     c.update(csrf(request))
-    return render(request, 'loggedin.html', c )
-
+    user_type = request.POST.get('user_type', False)
+    if user_type == 'I':
+        return render(request, 'html5up/investor.html', c )
+    elif user_type == 'C':
+        return render(request, 'html5up/company.html', c )
+    else:
+        return render(request, 'html5up/investor.html', c)
 
 def invalid(request):
-    return render(request, 'invalid.html')
+    return render(request, 'html5up/invalid.html')
 
 @login_required
 def loggedout(request):
     logout(request)
-    return render(request, 'logout.html')
+    return render(request, 'html5up/loggedout.html')
