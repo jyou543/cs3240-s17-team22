22b99c20198eca4f94b61be4de29e1a612f831c4
Neel Patel <np2ch@virginia.edu>
2017-04-27 09:43:09 -0400

    Bug fixes for SM and messaging

--- Fintech/account/models.py
+++ Fintech/account/models.py
@@ -3,29 +3,12 @@ from django.contrib.auth.models import User
 
 class CustomUser(models.Model):
     user = models.OneToOneField(User)
-    #to store the name of the user
-    # username = models.CharField(max_length=100)
-    #to store the password of the user
-    # email = models.CharField(max_length=100)
-    #
-    # password = models.CharField(max_length=100)
     type_choice = (
         ("C", "Company User"),
         ('I', "Investor User"),
     )
     is_SiteManager = models.BooleanField(default=False)
     user_type = models.CharField(max_length=1, choices=type_choice, default='C')
-    # if user_type == 'C':
-    #     class Meta:
-    #         permissions = (
-    #             ("manage_reports", "Can manage reports"),
-    #             ("view_reports", "Can View reports"),
-    #         )
-    # elif user_type == 'I':
-    #     class Meta:
-    #         permissions = (
-    #             ("view_reports", "view reports")
-    #         )
     publicKey=models.TextField(blank=True)
     privateKey=models.TextField(blank=True)
 
