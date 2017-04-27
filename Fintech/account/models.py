9d53c19668ce7a3be7c0400c61696a9ec7796207
Jeffery You <jy9vm@virginia.edu>
2017-04-27 14:44:03 -0400

    Fixed account string

--- Fintech/account/models.py
+++ Fintech/account/models.py
@@ -14,14 +14,9 @@ class CustomUser(models.Model):
 
 #this returns the name of the user when the object of user is printed
     def __str__(self):
-<<<<<<< HEAD
         sm = "False"
         if self.user.customuser.is_SiteManager:
             sm = "True"
             return self.user.get_username()+ ": " + "Site Manager"
         #return self.user.get_username() + ": " + (self.user.customuser.get_user_type_display()) + sm
         return self.user.get_username() + ": " + (self.user.customuser.get_user_type_display())
-
-=======
-        return self.user.get_username() 
->>>>>>> origin/master
